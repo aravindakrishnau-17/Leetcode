088c319bfaf68a5e7c11b357904e48ee7e174d25
Aravind Krishna <aravinda.krishna.u@gmail.com>
2026-01-18 02:46:05 +0530

    Time: 3 ms (80.58%) | Memory: 20.7 MB (8.43%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -8,7 +8,7 @@ class Solution:
                     return [i+1,j+1]
         return []
         """
-
+        """
         for i in range(len(numbers)):
             l,r = i+1, len(numbers)-1
             tmp = target - numbers[i]
@@ -21,4 +21,25 @@ class Solution:
                 else:
                     l=mid + 1
         return []
+        """
+        """
+        mp = defaultdict(int)
+        for i in range(len(numbers)):
+            tmp = target - numbers[i]
+            if mp[tmp]:
+                return [mp[tmp], i+1]
+            mp[numbers[i]] = i + 1
+        return []
+        """
+        l,r = 0, len(numbers)- 1
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+            if target < curSum:
+                r -= 1
+            elif target > curSum:
+                l += 1
+            else:
+                return [l+1 , r+1]
+        return []
+            
         
